b51356b640b29b991c2bb53016616ac33794c68b
Irusilda <irina.lyubodeeva@yandex.ru>
2023-02-07 14:58:59 +0300

    parse data

--- app/src/main/java/com/example/myweatherapp/MainActivity.kt
+++ app/src/main/java/com/example/myweatherapp/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.myweatherapp
 
-import android.app.DownloadManager.Request
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.myweatherapp.databinding.ActivityMainBinding
+import org.json.JSONObject
 
 const val API_KEY = "29dd79570f01448b8b670632230702"
 class MainActivity : AppCompatActivity() {
@@ -28,6 +28,7 @@ class MainActivity : AppCompatActivity() {
             url,
             {response ->
                 Log.d("MyLog", "Response: $response")
+                parseData(response)
             },
             {
                 Log.d("MyLog", "Error: $it")
@@ -35,5 +36,15 @@ class MainActivity : AppCompatActivity() {
         )
         queue.add(stringRequest)
 
+    }
+
+    private fun parseData(result: String?) {
+        val responseMainObject = result?.let { JSONObject(it) }
+        val itemDayWeather = OneDayData(
+            responseMainObject?.optJSONObject("current")?.getString("temp_c"),
+            responseMainObject?.optJSONObject("current")?.getJSONObject("condition")?.getString("text"),
+            responseMainObject?.optJSONObject("current")?.getString("humidity")
+        )
+        Log.d("MyLog", "$itemDayWeather")
     }
 }
---
+++ app/src/main/java/com/example/myweatherapp/OneDayData.kt
@@ -0,0 +1,8 @@
+package com.example.myweatherapp
+
+data class OneDayData(
+
+    val current_temp: String?,
+    val condition_text: String?,
+    val current_humidity: String?
+)
